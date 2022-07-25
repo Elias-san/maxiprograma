3c8a7ded82ac55a9d2fe3a6c2785a2d278d6d18d
Elias <elias.smartin@gmail.com>
2022-07-25 12:38:12 -0300

    Ejercicio 4

---
+++ Guia1/js/Ejercicio4.js
@@ -0,0 +1,6 @@
+
+let km,velocidad
+
+km= prompt("Ingrese kilometros entre ciudades: ")
+velocidad= prompt("Ingrese velocidad promedio: ")
+document.write("El tiempo de llegada es: ", km / velocidad)

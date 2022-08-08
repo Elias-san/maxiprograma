836b8abf45d737f74e5ea1273fd7e170144ad722
Elias <elias.smartin@gmail.com>
2022-08-07 22:10:33 -0300

    Metros cubiertos

---
+++ Guia1/cpp/ejercicio7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    float cubiertos,total,sincubrir;
+
+    cout << "Ingrese metros totales: ";
+    cin >> totales;
+    cout << "Ingrese metros cubiertos: ";
+    cin >> cubiertos;
+
+    return 0;
+}

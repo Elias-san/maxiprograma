eee4f5f7d68a9288a76d769e7caa3e9767df7629
Elias <elias.smartin@gmail.com>
2022-08-11 15:35:07 -0300

    Aritmetica de numeros

---
+++ Guia1/cpp/ejercicio10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+
+    cout << "Ingrese primer numero: " << endl;
+    cin >> num1;
+    cout << "Ingrese segundo numero: " << endl;
+    cin >> num2;
+
+    cout << "La suma de ambos numeros es: " << num1 + num2 << endl; 
+    cout << "La resta de ambos numeros es: " << num1 - num2 << endl;
+    cout << "El producto de ambos numeros es: " << num1 * num2 << endl;
+    return 0;
+}
